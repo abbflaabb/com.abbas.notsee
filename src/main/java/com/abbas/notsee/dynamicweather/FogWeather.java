37fdcca23f79c8854b5e3f0c97507d44b45902dd
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-16 19:43:25 +0300

    Create FogWeather.java

---
+++ src/main/java/com/abbas/notsee/dynamicweather/FogWeather.java
@@ -0,0 +1,15 @@
+package com.abbas.notsee.dynamicweather;
+
+import org.bukkit.World;
+
+public class FogWeather implements Weather {
+    @Override
+    public void apply(World world, int duration) {
+        // Implement your logic to create fog effect
+    }
+
+    @Override
+    public void clear(World world) {
+        // Implement your logic to clear fog effect
+    }
+}
