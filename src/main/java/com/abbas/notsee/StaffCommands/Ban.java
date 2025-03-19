98a053e095f19928c420745c99a0ff55005780de
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-19 17:47:20 +0300

    Make 8% Ban.java

--- src/main/java/com/abbas/notsee/StaffCommands/Ban.java
+++ src/main/java/com/abbas/notsee/StaffCommands/Ban.java
@@ -10,6 +10,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
 public class Ban implements CommandExecutor {
 
     public Ban() {
@@ -27,37 +33,40 @@ public class Ban implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("ban")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (!player.hasPermission("NotSee.Ban")) {
-                    player.sendMessage(ColorUtils.translateColorCodes("&cYou do not have permission to use this command!"));
-                    return true;
-                }
-
-                if (args.length < 1) {
-                    player.sendMessage(ColorUtils.translateColorCodes("&cUsage: /ban <player> [reason]"));
-                    return true;
-                }
-
-                Player target = Bukkit.getPlayer(args[0]);
-                if (target == null) {
-                    player.sendMessage(ColorUtils.translateColorCodes("&cPlayer not found!"));
-                    return true;
-                }
-
-                FileConfiguration config = Config.getConfig();
-                String defaultReason = config.getString("ban.default-reason", "&cBanned by Admin!");
-                String reason = defaultReason;
-                if (args.length > 1) {
-                    reason = String.join(" ", args).substring(args[0].length()).trim();
-                }
-
-                Bukkit.getBanList(BanList.Type.NAME).addBan(target.getName(), reason, null, player.getName());
-                target.kickPlayer(ColorUtils.translateColorCodes("&cYou are Banned!\nReason: " + reason));
-
-                player.sendMessage(ColorUtils.translateColorCodes("&aSuccessfully banned " + target.getName() + " for: " + reason));
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ColorUtils.translateColorCodes("&cUsage: /ban <player> [reason]"));
                 return true;
             }
+
+            Player player = (Player) sender;
+            if (!player.hasPermission("NotSee.Ban")) {
+                player.sendMessage(ColorUtils.translateColorCodes("&cYou do not have permission to use this command!"));
+                return true;
+            }
+
+            if (args.length < 1) {
+                player.sendMessage(ColorUtils.translateColorCodes("&cUsage: /ban <player> [reason]"));
+                return true;
+            }
+
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target == null) {
+                player.sendMessage(ColorUtils.translateColorCodes("&cPlayer not found!"));
+                return true;
+            }
+
+            FileConfiguration config = Config.getConfig();
+            String defaultReason = config.getString("ban.default-reason", "&cBanned by Admin!");
+            String reason = defaultReason;
+            if (args.length > 1) {
+                reason = String.join(" ", args).substring(args[0].length()).trim();
+            }
+
+            Bukkit.getBanList(BanList.Type.NAME).addBan(target.getName(), reason, null, player.getName());
+            target.kickPlayer(ColorUtils.translateColorCodes("&cYou are Banned!\nReason: " + reason));
+
+            player.sendMessage(ColorUtils.translateColorCodes("&aSuccessfully banned " + target.getName() + " for: " + reason));
+            return true;
         }
         return false;
     }
